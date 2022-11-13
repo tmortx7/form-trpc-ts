19c27cdae84d89dbe5c2f20ead58631ff8606202
x7eo <99x7eo@gmail.com>
2022-11-13 11:10:19 -0700

    prpops

--- src/components/layout/Teams.tsx
+++ src/components/layout/Teams.tsx
@@ -1,7 +1,19 @@
-const Teams = () => {
+import { ITeams } from "../../pages";
+
+const Teams = (props) => {
+  const items = props.items;
+
+  const listItems = items.map(({ id, alias }: ITeams) => (
+    <li key={id}>
+      <button className="btn btn-secondary ml-2 mb-2 w-16  ">{alias}</button>
+    </li>
+  ));
+
   return (
-    <div className="col-start-1 row-start-1 row-span-4 bg-neutral">
-      <div className="">value</div>
+    <div className="col-start-1 row-start-1 row-span-4 bg-neutral ">
+      <div className="">
+        <ul className="">{listItems}</ul>
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,30 @@ import Content from "../components/layout/Content";
 import Header from "../components/layout/Header";
 import Teams from "../components/layout/Teams";
 
+export type ITeams = {
+  id: number;
+  alias: string;
+};
+
 const HomePage = () => {
+  const items: ITeams[] = [
+    {
+      id: 1,
+      alias: "ls",
+    },
+    {
+      id: 2,
+      alias: "sls",
+    },
+    {
+      id: 3,
+      alias: "fs",
+    },
+  ];
   return (
     <div>
       <AppLayout>
-        <Teams />
+        <Teams items={items} />
         <Header />
         <Channels />
         <Content />
